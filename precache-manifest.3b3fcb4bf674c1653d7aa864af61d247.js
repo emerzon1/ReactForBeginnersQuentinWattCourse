c1295d909920d6f6784324325d02e28e6497f322
Evan Merzon <evandaniel160@yahoo.com>
2020-10-11 22:25:04 -0400

    Updates

---
+++ precache-manifest.3b3fcb4bf674c1653d7aa864af61d247.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d9e31dce5186dc6a59701580ef671f",
+    "url": "/MomBday/index.html"
+  },
+  {
+    "revision": "242fb330f9e51b3451d6",
+    "url": "/MomBday/static/css/main.42c1e69d.chunk.css"
+  },
+  {
+    "revision": "381ed955600a019ecef7",
+    "url": "/MomBday/static/js/2.d1d731e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MomBday/static/js/2.d1d731e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "242fb330f9e51b3451d6",
+    "url": "/MomBday/static/js/main.85ee4918.chunk.js"
+  },
+  {
+    "revision": "af85ff914b784e5462b6",
+    "url": "/MomBday/static/js/runtime-main.0061392e.js"
+  }
+]);
